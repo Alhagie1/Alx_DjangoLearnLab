e0e55b21d649a1bb817f3cad2f0a27e12ff4b2fc
alhagie nyang <alhagienyang13@gmail.com>
2025-09-14 00:30:31 +0000

    Deep dive into Django Models and Views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,7 +9,6 @@ Role_Choices = (
 )
 class Author(models.Model):
     name = models.CharField(max_length=100)
-
 class Book(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -27,4 +26,4 @@ class UserProfile(models.Model):
     role = models.CharField(max_length= 100, choices = Role_Choices)
 
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return self.name
