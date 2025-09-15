1a542d36fce5ec5e1a2f464bbe8d771f3ec88ce0
alhagie nyang <alhagienyang13@gmail.com>
2025-09-15 15:50:15 +0000

    Implementing a custom user model in Django

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book,CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "publication_year"]
     list_filter = ["publication_year"]
     search_fields = ["title", "author"]
 admin.site.register(Book,BookAdmin)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('date_of_birth', 'profile_photo',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,52 @@
 from django.db import models
-
+from django.db import models
+from django.contrib.auth.models import User, AbstractUser,BaseUserManager
+from django.contrib.auth.models import Permission
+from django.conf import settings
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE) 
+    role = models.CharField(max_length= 100, choices = Role_Choices)
+    
+    def __str__(self):
+        return self.user
+    
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'date_of_birth']
+    def __str__(self):
+        return self.email
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email must be found")
+        email = self.normalize_email(email)
+        user = self.model(email = email)
+        user.set_password(password)
+        user.save(using = self._db)
+        return self.create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault(staff = True)
+        extra_fields.setdefault(active = True)
+        extra_fields.setdefault(is_superuser = True)
+        if extra_fields.get("is-staff") is not True:
+            raise ValueError("superuser must have is_staff = True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("superuser must get is_superuser = True")
+        return self.create_superuser(email, password, **extra_fields)
+    
+
+
+
+
+
