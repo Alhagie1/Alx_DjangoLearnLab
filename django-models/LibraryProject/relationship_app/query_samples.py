c648f4e92e83174f34aa9d9afd613570af1d2e62
alhagie nyang <alhagienyang13@gmail.com>
2025-09-18 12:34:10 +0000

    django models and views

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,11 +4,11 @@ from .models import Book, Librarian, Library
 
 book = Book.objects.filter(author = "NjolTech")
 
-library = Library.objects.get(name="Public Libary")
+library = Library.objects.get(library_name="Public Libary")
 books = Library.books.all()
 
 try:
-   librarian = Librarian.objects.get(name = "John Rix")
+   librarian = Librarian.objects.get(librarian_name = "John Rix")
 except Librarian.DoesNotExist:
    print("model does not exist")
 except Librarian.MultipleObjectsReturned:
