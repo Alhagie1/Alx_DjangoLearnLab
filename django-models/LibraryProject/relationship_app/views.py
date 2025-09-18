e7ae82c8b75d73809f368d630bc8435bc7477271
alhagie nyang <alhagienyang13@gmail.com>
2025-09-18 10:44:54 +0000

    Django Models and Views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Book
 from django.views.generic import ListView
 from django.contrib.auth import login
@@ -8,9 +8,18 @@ def list_books(request):
     all_books = Book.objects.all()
     return render (request, "list_books.html",{"all_books": all_books})
 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
 class LibraryDetails(ListView):
     model = Book
     template_name = "library_details.html"
     context_object_name = "books"
-    
     
