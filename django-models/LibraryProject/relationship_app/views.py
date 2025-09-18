4a9a9e2581a51236e6d5f829e501e2af77dbea3b
alhagie nyang <alhagienyang13@gmail.com>
2025-09-18 10:51:47 +0000

    Django Views and Models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 def list_books(request):
     """List the details of the book"""
     all_books = Book.objects.all()
-    return render (request, "list_books.html",{"all_books": all_books})
+    return render (request, "relationship_app/list_books.html",{"all_books": all_books})
 
 
 def register(request):
