e78187ff4064c94dee0fe1e25a9101fa339bfb63
alhagie nyang <alhagienyang13@gmail.com>
2025-09-20 20:29:23 +0000

    Building api endpoint with Django Rest Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,10 @@
 
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'books_all', BookList, basename='book_all')
 urlpatterns = [
-    path("Book/", BookList.as_view(), name="Book")
+    path("books/", BookList.as_view(), name="book-list"),
+    path('', include(router.urls)),
 ]
