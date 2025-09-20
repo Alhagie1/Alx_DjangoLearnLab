fa7dd6714eb142a4a07361c3502090b0edc033fd
alhagie nyang <alhagienyang13@gmail.com>
2025-09-20 20:34:41 +0000

    Building api endpoint with Django Rest Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path, include
-from .views import BookList
+from .views import BookViewSet
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register(r'books_all', BookList, basename='book_all')
+router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
-    path("books/", BookList.as_view(), name="book-list"),
+    path("books/", BookViewSet.as_view(), name="book-list"),
     path('', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookViewSet(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
